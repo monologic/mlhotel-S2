4de2a28ec4b464f3aca120acce359c8073a6db5b
xxmaucoxx <btr_kirito1@hotmail.com>
2016-04-13 17:11:11 -0500

    obteniendo los datos de iconos
    capturando datos con angular

--- public/plugins/js/controllers/habtipogalController.js
+++ public/plugins/js/controllers/habtipogalController.js
@@ -12,15 +12,29 @@ app.controller('habtipogalController', function($scope,$http, $routeParams) {
             });
            
         }
-
+    var iconos;
     $scope.gethabitacionG= function () { 
             $http.get('admin/gethabitaciones/'+IdHab).then(function successCallback(response) {
                 $scope.habfotos=response.data;
                 $scope.galeria = response.data[0].habtipofotos;
+                iconos=response.data[0].habtipo_serviciointernos;
             }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
             });
            
         }
+
+    $scope.GuardarIcon= function () { 
+            for (var i = 0; i < iconos.length; i++) {
+                if ($('#'+iconos[i].id).prop('checked'))
+                    iconos[i].estado='true';
+            $http.put('admin/gethabitaciones/'+IdHab).then(function successCallback(response) {
+            }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            });
+            }          
+        }
+
 });
