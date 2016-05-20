bd0de412b63ae04803850668ba50e5f8dc2802e3
Redlein <redlein7@gmail.com>
2016-05-20 11:42:43 -0500

    Modificar title

--- resources/views/dash/admin.blade.php
+++ resources/views/dash/admin.blade.php
@@ -5,7 +5,17 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador</title>
+    <title>
+        @if (Auth::user()->usuariotipo->nombre == "Administrador")
+            Administrador
+        @endif
+        @if (Auth::user()->usuariotipo->nombre=="Root")
+            Root
+        @endif
+        @if (Auth::user()->usuariotipo->nombre != "Administrador" && Auth::user()->usuariotipo->nombre != "Root")
+            Recepcionista
+        @endif
+    </title>
     <meta name="description" content="Blueprint: A basic template for a responsive multi-level menu" />
     <meta name="keywords" content="blueprint, template, html, css, menu, responsive, mobile-friendly" />
     <meta name="author" content="Codrops" />
