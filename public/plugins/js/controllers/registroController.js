d1bfbfe4c1993ac44677d8e354411700bf55d3e5
Redlein <redlein7@gmail.com>
2016-06-15 11:22:07 -0500

    impresion

--- public/plugins/js/controllers/registroController.js
+++ public/plugins/js/controllers/registroController.js
@@ -397,4 +397,12 @@ app.controller('registroController', function($scope,$http , $routeParams) {
             } 
         }
     });
+
+    $scope.printDiv = function(divName) {
+        var printContents = document.getElementById(divName).innerHTML;
+        var popupWin = window.open('', '_blank', 'width=300,height=300');
+        popupWin.document.open();
+        popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
+        popupWin.document.close();
+    }
 });
