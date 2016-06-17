c3e1490d9ea4b6e2e84aa6dfcc3dc806006585e4
xxmaucoxx <btr_kirito1@hotmail.com>
2016-06-17 14:19:13 -0500

    guardando servicios

--- public/plugins/js/controllers/serviciosController.js
+++ public/plugins/js/controllers/serviciosController.js
@@ -31,7 +31,6 @@ app.controller('serviciosController', function($scope,$http, $routeParams,$locat
             }
 
     });
-
     $scope.getServiciosD= function () { 
             $http.get('admin/CategoriaServicio/'+ IdCat).then(function successCallback(response) {
                 $scope.infoser=response.data[0];  
@@ -45,12 +44,16 @@ app.controller('serviciosController', function($scope,$http, $routeParams,$locat
         }
     $scope.main = function (){
         $http.get('admin/getmainservices').then(function successCallback(response) {
-                $scope.main=response.data[0];  
+                $scope.main=response.data[0];
             }, function errorCallback(response) {
             // called asynchronously ialertf an error occurs
             // or server returns response with an error status.
             });
     }
+     $scope.$on('ngRepeatFinished3', function(ngRepeatFinishedEvent) {
+        $('#contmain').html($scope.main=response.data[0].contenido)
+    });
+
         $scope.goTo2 = function(data) {
          catId = data.id;
         $location.url('/Servicios/' + catId);
